7c40c2124312f207c1a11e75f0c7a207f586fadd
Mazen <mazen.mohamed.967@gmail.com>
2020-05-02 09:15:06 +0200

    Add Dice Flipping behavior

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,43 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftNum = 1, rightNum = 1;
+  void flipDice() {
+    setState(() {
+      leftNum = Random().nextInt(6) + 1;
+      rightNum = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                flipDice();
+              },
+              child: Image.asset('images/dice$leftNum.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                flipDice();
+              },
+              child: Image.asset('images/dice$rightNum.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
